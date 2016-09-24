e40c6f97da099a180d91dcc00c8b40ecb1dcad75
unknown <xiaohaogreat@qq.com>
2016-09-24 18:13:25 +0800

    use another func

---
+++ Symmetric Tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isMirror(self,p,q):
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        elif p.val != q.val: 
+            return False
+        else:
+            return self.isMirror(p.left, q.right) and self.isMirror(p.right, q.left)
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        return self.isMirror(root.left, root.right)
