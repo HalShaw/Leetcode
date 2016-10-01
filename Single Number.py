c53c48207f4cb6366e18b1b942def256c7d1472f
HalShaw <xiaohaogreat@qq.com>
2016-10-01 10:09:30 +0800

    Update Single Number.py

--- Single Number.py
+++ Single Number.py
@@ -8,3 +8,7 @@ class Solution(object):
         for i in range(1,len(nums)):
             a^=nums[i]#所有元素异或，相同的异或后为0,0与任何数异或都为它本身
         return a
+    '''不使用异或,使用set
+        s1 = set(nums)
+        a2 = sum(s1)*2-sum(nums)
+        return a2 ''' 
